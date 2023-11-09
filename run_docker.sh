2ebfd5c7ee9046802272bed15aeb06378bace867
0Unkn0wn <37101806+0Unkn0wn@users.noreply.github.com>
2023-11-09 22:31:24 +0100

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,4 @@
 
 echo Running jax_jetson docker image
 
-nvidia-docker run -it --rm --net=host --runtime nvidia --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix test/l4t:0.2 bin/bash
+nvidia-docker run -it --rm --net=host --runtime nvidia --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix l4t/jax_ros2:0.3 bin/bash
