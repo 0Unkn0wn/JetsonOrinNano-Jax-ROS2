0b8794fd089e5fccd0136822a29c9f93a2dd6d9d
0Unkn0wn <37101806+0Unkn0wn@users.noreply.github.com>
2023-11-09 22:07:49 +0100

    Update docker_jax_ros2_build.sh
    Added docker privileges

--- docker_jax_ros2_build.sh
+++ docker_jax_ros2_build.sh
@@ -1,6 +1,10 @@
 ### Building docker
 echo Building the jax-ros2 docker image
 
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
 docker pull dustynv/ros:humble-desktop-l4t-r35.4.1
 
 docker build -f Dockerfile_jax_ros2 -t l4t/jax_ros2:0.3 .
