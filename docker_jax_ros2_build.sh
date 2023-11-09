b393cdbe9515b84b000dac7e72819b16a658ec29
0Unkn0wn <37101806+0Unkn0wn@users.noreply.github.com>
2023-11-09 22:10:31 +0100

    Update docker_jax_ros2_build.sh

--- docker_jax_ros2_build.sh
+++ docker_jax_ros2_build.sh
@@ -5,6 +5,8 @@ sudo groupadd docker
 
 sudo usermod -aG docker $USER
 
+newgrp docker
+
 docker pull dustynv/ros:humble-desktop-l4t-r35.4.1
 
 docker build -f Dockerfile_jax_ros2 -t l4t/jax_ros2:0.3 .
