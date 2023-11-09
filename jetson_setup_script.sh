cd0a13d3cd16df983efce8b3a2f9830455531980
0Unkn0wn <37101806+0Unkn0wn@users.noreply.github.com>
2023-11-09 20:07:39 +0100

    Fixed a bug in the jetson setup script
    Added mixing curl

--- jetson_setup_script.sh
+++ jetson_setup_script.sh
@@ -6,6 +6,10 @@ echo Starting the installation process
 
 echo Installing docker
 
+sudo apt update
+
+sudo apt-get install -y curl
+
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
