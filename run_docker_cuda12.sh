a21da0e4bfeca3e52b6657070ae11ce7f306bbe3
0Unkn0wn <37101806+0Unkn0wn@users.noreply.github.com>
2023-11-10 14:38:33 +0100

    Added files for cuda 12 version
    Added dockerfile with cuda 12 fix taken from https://github.com/dusty-nv/jetson-containers/issues/258#issuecomment-1804648224
    and added 2 scripts for building and running the cuda 12 container
    and added a jax performance jupyter python script to test the container and to see the difference in performance

---
+++ run_docker_cuda12.sh
@@ -0,0 +1,5 @@
+## Run docker jax_jetson image
+
+echo Running jax_jetson docker image
+
+nvidia-docker run -it --rm --net=host --runtime nvidia --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix l4t/jax_ros2_cuda12:0.1 bin/bash
