4151a801443dd72c0a609ce06027649df93814fb
0Unkn0wn <37101806+0Unkn0wn@users.noreply.github.com>
2023-11-20 15:32:05 +0100

    Update run_docker_cuda12.sh
    updated running script to fix x11 forwarding over ssh connection and added the ability to pass USB devices as well.

--- run_docker_cuda12.sh
+++ run_docker_cuda12.sh
@@ -2,4 +2,4 @@
 
 echo Running jax_jetson docker image
 
-docker run -it --rm --net=host --runtime nvidia --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix l4t/jax_ros2_cuda12:0.1 bin/bash
+docker run -it --rm --net=host --runtime nvidia --gpus all -e DISPLAY=127.0.0.1:11.0 --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v /tmp/.X11-unix/:/tmp/.X11-unix -v /dev:/dev -v /run/udev:/run/udev:ro --device-cgroup-rule='c 188:* rmw' --device-cgroup-rule='c 81:* rmw' l4t/jax_ros2_cuda12:0.1 bin/bash
