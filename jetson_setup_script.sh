fc2843ebd51960484dd362f184422f9f53c0e29f
0Unkn0wn <37101806+0Unkn0wn@users.noreply.github.com>
2023-11-21 12:42:34 +0100

    Update jetson_setup_script.sh
    removed unnecessary line

--- jetson_setup_script.sh
+++ jetson_setup_script.sh
@@ -68,7 +68,6 @@ set-environment -g -u PROMPT_COMMAND \n' >> ~/.tmux.conf
 echo Installing jtop or jetson-stats
 sudo apt-get install python3-pip
 sudo pip3 install -U jetson-stats
-sudo systemctl status jetson_stats.service
 
 echo Installing nano
 sudo apt install -y nano
