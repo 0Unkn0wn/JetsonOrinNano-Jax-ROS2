04f69a372472040f651f0141400af60509388448
0Unkn0wn <37101806+0Unkn0wn@users.noreply.github.com>
2023-11-29 10:27:11 +0100

    Update run_docker_cuda12.sh

--- run_docker_cuda12.sh
+++ run_docker_cuda12.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 ## Run docker jax_jetson image
 
-echo Running jax_jetson docker image
+echo $'\nRunning jax_jetson docker image'
 
 docker run -it --rm --net=host --runtime nvidia --gpus all -e DISPLAY=$DISPLAY --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v /tmp/.X11-unix/:/tmp/.X11-unix -v /dev:/dev -v /run/udev:/run/udev:ro --device-cgroup-rule='c 188:* rmw' --device-cgroup-rule='c 81:* rmw' l4t/jax_ros2_cuda12:0.1 bin/bash
