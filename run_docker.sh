b6100124a14811b6c7bbba3cdae5a499b20b9aa2
0Unkn0wn <37101806+0Unkn0wn@users.noreply.github.com>
2023-11-29 10:30:15 +0100

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,6 @@
-## Run docker jax_jetson image
 #!/bin/bash
+
+## Run docker jax_jetson image
+
 echo $'\nRunning jax_jetson docker image'
 docker run -it --rm --net=host --runtime nvidia --gpus all -e DISPLAY=$DISPLAY --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v /tmp/.X11-unix/:/tmp/.X11-unix -v /dev:/dev -v /run/udev:/run/udev:ro --device-cgroup-rule='c 188:* rmw' --device-cgroup-rule='c 81:* rmw' l4t/jax_ros2:0.3 bin/bash
